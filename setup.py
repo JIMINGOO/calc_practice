78379bf424398724289af2d3989a0e7d2983adc1
JIMINGOO <100738522+JIMINGOO@users.noreply.github.com>
2022-05-04 09:19:52 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data = True,
+  name='mycalc0001',
+  version='0.0.1',
+  description='oss-dev my calculator 0001',
+  author='JIMINGOO',
+  author_email='greenify_88@naver.com',
+  url = "https://github.com/JIMINGOO/calc_practice",
+  download_url = "",
+  install_requires=['pytest'],
+  long_description = 'oss-dev calculator python module',
+  long_description_content_type = 'text/markdown',
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ],
+)
